657cb73502d7c558700482c7bf8e14351a42c396
MarvinChung <marvin852316497@gmail.com>
2021-12-24 16:07:04 +0800

    feat: Upload last project
    Add graded system integration project to github

---
+++ Project9-System-Integration/ros/devel/share/gennodejs/ros/dbw_mkz_msgs/msg/_index.js
@@ -0,0 +1,58 @@
+
+"use strict";
+
+let BrakeInfoReport = require('./BrakeInfoReport.js');
+let WatchdogCounter = require('./WatchdogCounter.js');
+let GearCmd = require('./GearCmd.js');
+let WheelPositionReport = require('./WheelPositionReport.js');
+let BrakeCmd = require('./BrakeCmd.js');
+let ThrottleInfoReport = require('./ThrottleInfoReport.js');
+let TirePressureReport = require('./TirePressureReport.js');
+let BrakeReport = require('./BrakeReport.js');
+let ThrottleReport = require('./ThrottleReport.js');
+let SurroundReport = require('./SurroundReport.js');
+let ParkingBrake = require('./ParkingBrake.js');
+let Gear = require('./Gear.js');
+let SteeringReport = require('./SteeringReport.js');
+let Misc1Report = require('./Misc1Report.js');
+let GearReject = require('./GearReject.js');
+let SteeringCmd = require('./SteeringCmd.js');
+let TurnSignalCmd = require('./TurnSignalCmd.js');
+let TwistCmd = require('./TwistCmd.js');
+let FuelLevelReport = require('./FuelLevelReport.js');
+let WheelSpeedReport = require('./WheelSpeedReport.js');
+let AmbientLight = require('./AmbientLight.js');
+let GearReport = require('./GearReport.js');
+let ThrottleCmd = require('./ThrottleCmd.js');
+let Wiper = require('./Wiper.js');
+let TurnSignal = require('./TurnSignal.js');
+let HillStartAssist = require('./HillStartAssist.js');
+
+module.exports = {
+  BrakeInfoReport: BrakeInfoReport,
+  WatchdogCounter: WatchdogCounter,
+  GearCmd: GearCmd,
+  WheelPositionReport: WheelPositionReport,
+  BrakeCmd: BrakeCmd,
+  ThrottleInfoReport: ThrottleInfoReport,
+  TirePressureReport: TirePressureReport,
+  BrakeReport: BrakeReport,
+  ThrottleReport: ThrottleReport,
+  SurroundReport: SurroundReport,
+  ParkingBrake: ParkingBrake,
+  Gear: Gear,
+  SteeringReport: SteeringReport,
+  Misc1Report: Misc1Report,
+  GearReject: GearReject,
+  SteeringCmd: SteeringCmd,
+  TurnSignalCmd: TurnSignalCmd,
+  TwistCmd: TwistCmd,
+  FuelLevelReport: FuelLevelReport,
+  WheelSpeedReport: WheelSpeedReport,
+  AmbientLight: AmbientLight,
+  GearReport: GearReport,
+  ThrottleCmd: ThrottleCmd,
+  Wiper: Wiper,
+  TurnSignal: TurnSignal,
+  HillStartAssist: HillStartAssist,
+};
---
+++ Project9-System-Integration/run_docker.sh
@@ -0,0 +1,2 @@
+docker run -p 4567:4567 -v $PWD:/capstone -v /tmp/log:/root/.ros/ --rm -it capstone
+# docker run -p 4567:4567 -v $PWD:/capstone -v /tmp/log:/root/.ros/ -it capstone
